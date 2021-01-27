5f9e2a0c6e31ef50d0db27896978b110b6d28f65
Shuvo Paul <shuvopaul@yahoo.com>
2021-01-27 18:41:37 +0600

    ElementalCmsEditLinkExtension added

---
+++ src/Extensions/ElementalCmsEditLinkExtension.php
@@ -0,0 +1,28 @@
+<?php
+namespace LazyCoders\Elemental\Elements;
+
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\CMS\Model\SiteTree;
+
+class ElementalCmsEditLinkExtension extends DataExtension {
+
+    public function updateCMSEditLink(&$link): void
+    {
+        if (!$this->owner->inlineEditable()) {
+            $page = $this->owner->getPage();
+
+            if (!$page || $page instanceof SiteTree) {
+                return;
+            }
+
+            // As non-page DataObject's are managed via GridFields, we have to grab their CMS edit URL
+            // and replace the trailing /edit/ with a link to the nested ElementalArea edit form
+            $relationName = $this->owner->getAreaRelationName();
+            $link = preg_replace(
+                '/edit\/?$/',
+                "ItemEditForm/field/{$relationName}/item/{$this->owner->ID}/edit/",
+                $page->CMSEditLink()
+            );
+        }
+    }
+}
